1c3143f8a223fd936844b47bf28c068e010d64cf
zhangguolin <zhangguolin-1990@163.com>
2020-01-07 20:43:50 +0800

    配置admin

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -1,3 +1,16 @@
-from django.contrib import admin
+# -*-coding:utf-8-*-
 
-# Register your models here.
+from django.contrib import admin
+from .models import Post, Category, Tag
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'status', 'is_nav', 'created_time']
+    fields = ['name', 'status', 'is_nav','owner']
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name', 'status', 'created_time']
+    fields = ['name', 'status']
